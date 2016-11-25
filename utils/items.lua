2d40f3a00c9e13d0f616d2dea3851edcf0e04f53
Nicolás Tamargo <Nicols Tamargo>
2016-11-25 15:08:48 +0200

    Refactored the shop to auto-create and filter from a given set of items

---
+++ utils/items.lua
@@ -0,0 +1,33 @@
+local items = { }
+
+table.insert(items, { 
+  id = 1,
+  type = "Dog",
+  name = "Thingy 1", 
+  image = "pictures/syda.png"} )
+table.insert(items, { 
+  id = 2,
+  type = "Dog",
+  name = "Thingy 2", 
+  image = "pictures/syda.png"} )
+table.insert(items, { 
+  id = 3,
+  type = "Cat",
+  name = "Thingy 3", 
+  image = "pictures/cat1.png"} )
+table.insert(items, { 
+  id = 4,
+  type = "Food",
+  name = "Thingy 4", 
+  image = "pictures/Xfullred.png"} )
+table.insert(items, { 
+  id = 5,
+  type = "Food",
+  name = "Thingy 5", 
+  image = "pictures/syda.png"} )
+table.insert(items, { 
+  type = "Toy",
+  name = "Thingy 6", 
+  image = "pictures/syda.png"} )
+
+return items
